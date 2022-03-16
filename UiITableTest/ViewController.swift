098cda20143e8fc6f83d52722ad64fa63b5e1a14
Евгений Крапивенко <ekrapivenko@gmail.com>
2022-03-16 16:35:57 +0300

    Initial Commit

---
+++ UiITableTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiITableTest
+//
+//  Created by Eugene Krapivenko on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
